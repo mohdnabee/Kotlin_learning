96de8f9da2dd00fdb187af2c93842fc1b89ff03f
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-17 19:47:49 +0530

    second program kt

---
+++ src/fprogram2.kt
@@ -0,0 +1,19 @@
+fun main(){
+    println(" Kotlin Variables ")
+
+
+    // syntax
+        //  var  variableName =  value
+
+    //  example
+     var name =  "Nabeel"
+    var birthyear = "2003"
+
+    println(name) //  print the value of name
+    println(birthyear)//  print the value of birthyear
+    println()
+    println("Variables are containers for storing data values.\n" +
+           
+            "To create a variable, use var or val, and assign a value to it with the equal sign (=):")
+
+}
