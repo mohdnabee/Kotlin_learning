bd59fca0e1b81b303cb946520c639e1f8c31165a
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-17 19:39:31 +0530

    first program kt

---
+++ src/program1.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    println("This is kotlin programing ")
+    println("This is my first program in kotlin")
+    println(3+3)
+
+    print("Hello World! ")
+    print("I am learning Kotlin. ")
+    print("It is awesome!")
+
+}
