151cb0396dac92c7afe0d3f91a813bebd848e8f5
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-17 23:51:29 +0530

    third program kt

--- src/p3.kt
+++ src/p3.kt
@@ -2,11 +2,32 @@ fun main(){
     //  variables
 
     var name : String =  "John" //  string
-    var birthyear :  Int =  1975 //  Int
+    val birthyear :  Int =  1975 //  Int
 
     println(name)
     println(birthyear)
 
 
     println("Kotlin is smart enough to understand that \"John\" is a String (text), and that 1975 is an Int (number) variable.")
+
+
+    // val newName = "Don" ||  val shows error
+    var newName =  "Don"
+    newName =  "boss"
+    println(newName)
+
+    val pi = 3.14159265359
+    println(pi)
+
+
+    val  firstName =  "Mohd"
+    val SecondName =  "Nabeel"
+
+    val fullName =  firstName + SecondName
+    println(fullName)
+
+
+    val x =  4
+    val y = 5
+    println("X +y is : "+(x+y))
 }
