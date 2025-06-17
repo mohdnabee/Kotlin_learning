88a1cc5e570a1cb4f5734e1d3f575eef74b64828
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-17 19:47:59 +0530

    third program kt

---
+++ src/p3.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //  variables
+
+    var name : String =  "John" //  string
+    var birthyear :  Int =  1975 //  Int
+
+    println(name)
+    println(birthyear)
+
+
+    println("Kotlin is smart enough to understand that \"John\" is a String (text), and that 1975 is an Int (number) variable.")
+}
