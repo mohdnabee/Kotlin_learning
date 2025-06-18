c0b9b51d1b9bbd1490727c9ba4b0408dfc2a0858
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-18 10:00:57 +0530

    Kotlin  When

---
+++ src/Conditional/WhenC/when2.kt
@@ -0,0 +1,17 @@
+package Conditional.WhenC
+
+fun main(){
+    val number =  5
+    val  result =  when(number){
+
+        1->"hello"
+        2->"bye"
+        3 -> "Good Day"
+        4 -> "happy"
+        5 ->"its Five"
+        6 ->"less 1 than 7 "
+        7 ->"last number"
+        else -> "not valid number"
+    }
+    println(result)
+}
