703214e7fcbcf7f47b6c3f7c7bfe6670755b4a7b
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-21 12:10:12 +0530

    Increnemt and decremnt oper

---
+++ src/questions/live score.kt
@@ -0,0 +1,30 @@
+package questions
+
+fun main (){
+    var  lives =  3
+    var score =  100
+    var level =  1
+    var coins =  5
+
+    //  operations :
+    lives--
+    lives--
+    coins++
+    coins++
+    coins++
+    lives++
+    level++
+    coins--
+    coins--
+    coins--
+    coins--
+
+     println("Game status : ")
+    println()
+    println("Lives $lives")
+    println("Score $score")
+    println("Level $level")
+    println("Coins $coins")
+
+
+}
