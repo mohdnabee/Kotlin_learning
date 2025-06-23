2bf57668628a3b4a17005e769e2b6c92d6e2169e
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-23 08:48:26 +0530

    Kotlin  course p1

---
+++ src/Kotlin Course Program/p1.kt
@@ -0,0 +1,10 @@
+package `Kotlin Course Program`
+
+class  p1{
+     companion object {
+          @JvmStatic
+          fun  main (args : Array<String>){
+              println("Hello world!")
+          }
+     }
+}
