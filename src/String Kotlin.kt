2fbe4d41554dac8ec1bba30d954e651cfd493680
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-26 19:10:42 +0530

    string in kotlin

---
+++ src/String Kotlin.kt
@@ -0,0 +1,23 @@
+fun main (){
+
+    var  str =  "Hello"
+    println(str)
+    str = "hii"
+    println(str)
+
+    var  para=  "Very  long Paragraphs can be stored inside of stings"
+    println(para)
+
+    var  name =  "Gamer"
+    name =  "you Are Coder"
+    println(name.lowercase())
+    println(name.uppercase())
+
+    println(name + " Yes")
+
+    var firstName =  "Mohd"
+    var secondName =  "Nabeel"
+    println(firstName + "\t" +secondName)
+
+    // \n  ,  \t,
+}
