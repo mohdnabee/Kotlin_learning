ef63e657c2adae39a02246d1fc8b46db114016ef
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-27 21:05:12 +0530

    coffe machine

---
+++ src/function coffe machine.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+//  call function
+    makeCoffee()
+
+}
+
+fun makeCoffee(){
+
+    println("Buy Beans")
+    println("Grind Beans")
+    println("heat water to  90°C.")
+    println("Add water")
+    println("Push hot water through the grinder beans ")
+
+}
