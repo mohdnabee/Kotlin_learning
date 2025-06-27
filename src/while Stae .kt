706c2c2fb806295f5300cf5daeada48fdadcfe5a
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-27 18:36:07 +0530

    while loop

---
+++ src/while Stae .kt
@@ -0,0 +1,13 @@
+fun main(){
+    var count =0
+
+//    while (true){
+//        //  so long this content will  be executed
+//    }
+
+    while (count <5){
+        println("count is $count" )
+        count ++
+    }
+    println("loop is done ")
+}
