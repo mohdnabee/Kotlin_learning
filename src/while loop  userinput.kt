8930de0f34db67cc96c9ae72d72369fc48b1ac96
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-27 18:35:57 +0530

    while loop user input

---
+++ src/while loop  userinput.kt
@@ -0,0 +1,11 @@
+fun main (){
+
+    
+    var userInput = readln()
+
+    while (userInput =="1"){
+        println("while loop executed")
+        userInput = readln()
+    }
+    println("loop is done")
+}
