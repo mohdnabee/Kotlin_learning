9181a9ee2509ab4413a88525b89a13f7779e24f1
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-28 13:20:39 +0530

    call class dog

---
+++ src/clasess/call Dog.kt
@@ -0,0 +1,9 @@
+package clasess
+
+fun main (){
+
+    //  creating an object/instance of the class dog
+    var myDog = Dog("Rock" ,"Husky")
+    println("${myDog.name } is a ${myDog.breed} ")
+
+}
