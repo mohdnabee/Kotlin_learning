e1cc6950cf5742132b74010588ddea0080e5ff6d
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-28 13:20:18 +0530

    class dog

---
+++ src/clasess/Dog.kt
@@ -0,0 +1,13 @@
+package clasess
+
+class Dog( val name : String , val  breed : String) {
+
+    init {
+        bark(name)
+    }
+
+    fun  bark  (name : String) {
+        println("$name Says woof! woof!")
+    }
+
+}
