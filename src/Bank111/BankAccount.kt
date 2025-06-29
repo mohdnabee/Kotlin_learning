bd96238b79ca38969924b6252d0ad34d982abaeb
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-29 23:24:31 +0530

    Bank account

---
+++ src/Bank111/BankAccount.kt
@@ -0,0 +1,20 @@
+package Bank111
+
+class BankAccount(var accountHolder : String , var balance: Double) {
+
+    private val  transactionHistory =  mutableListOf<String>()
+
+
+    fun deposit(amount : Double){
+
+    }
+
+    fun withdraw(amount: Double){
+
+    }
+
+    fun displayTransactionHistory(){
+
+    }
+
+}
