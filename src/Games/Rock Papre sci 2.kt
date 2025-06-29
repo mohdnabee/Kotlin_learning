d9b716dddeb6d2b70f68166bc3e561f8b105c172
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-30 00:18:44 +0530

    Rock  paper sci  v2.0

--- src/Games/Rock Papre sci 2.kt
+++ src/Games/Rock Papre sci 2.kt
@@ -1,50 +1,55 @@
 package Games
 
-fun main(){
+fun main() {
+    var playerScore = 0
+    var computerScore = 0
 
-    var computerChoice =""
-    var playerChoice = ""
-    println("Rock, Paper or Scissors? Enter your Choice!")
-    playerChoice =readln()
+    println("Welcome to Rock, Paper, Scissors Game!")
 
+    while (true) {
+        println("\nEnter Rock, Paper or Scissors (or type 'exit' to quit):")
+        val input = readln().trim().lowercase()
 
-    val  randomNumber =(1..3).random()
-
-    when (randomNumber) {
-        1 -> {
-            computerChoice ="Rock"
+        if (input == "exit") {
+            println("\nFinal Score => Player: $playerScore | Computer: $computerScore")
+            println("Thanks for playing!")
+            break
         }
-        2 -> {
-            computerChoice ="Paper"
+
+        val playerChoice = input.replaceFirstChar { it.uppercase() }
+        if (playerChoice != "Rock" && playerChoice != "Paper" && playerChoice != "Scissors") {
+            println("Invalid choice. Please enter Rock, Paper or Scissors.")
+            continue
         }
-        3 -> {
-            computerChoice ="Scissors"
+
+        val computerChoice = when ((1..3).random()) {
+            1 -> "Rock"
+            2 -> "Paper"
+            else -> "Scissors"
         }
+
+        println("Computer chose: $computerChoice")
+
+        val winner = when {
+            playerChoice == computerChoice -> "Tie"
+            playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+            playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+            playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+            else -> "Computer"
+        }
+
+        when (winner) {
+            "Tie" -> println("It's a tie!")
+            "Player" -> {
+                println("You won!")
+                playerScore++
+            }
+            "Computer" -> {
+                println("Computer won!")
+                computerScore++
+            }
+        }
+
+        println("Score => Player: $playerScore | Computer: $computerScore")
     }
-
-    println(computerChoice)
-
-    val  winner =  when{
-        playerChoice ==  computerChoice -> "Tie"
-
-        playerChoice == "Rock"     &&  computerChoice == "Scissors" ->"Player"
-        playerChoice == "Paper"    &&  computerChoice == "Rock"     -> "Player"
-        playerChoice == "Scissors" &&  computerChoice == "Paper"    -> "player"
-        else -> "Computer"
-    }
-
-//    if (winner == "Tie"){
-//        println("It's a tie")
-//    }else if (winner == "Player"){
-//        println("Player won!")
-//    }else{
-//        println("Computer won")
-//    }
-
-    if (winner == "Tie"){
-        println("It's a tie")
-    }else {
-        println(winner + " won!")
-    }
-
 }
