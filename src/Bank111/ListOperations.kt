b87ff6b8a702b775dba3d6b93efc4f09f9cf4d10
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-30 00:21:16 +0530

    list  operation

---
+++ src/Bank111/ListOperations.kt
@@ -0,0 +1,21 @@
+package Bank111
+fun main(){
+
+    val  fruitsList =  mutableListOf("Mango", "Orange", "Banana", "Pineapple", "Apple")
+
+    fruitsList.add("Watermelon")
+    fruitsList.remove("Banana")
+
+    val fruits= fruitsList.contains("Banana")
+    // if(hasRam){
+    if(fruits){
+        println("Has Banana in the list")
+    }else {
+        println("No Banana in the list")
+    }
+    println(fruitsList)
+
+
+    fruitsList.add(3,"Coconut")
+    println(fruitsList)
+}
