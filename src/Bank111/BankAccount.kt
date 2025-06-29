e6730b82dd28063e69643ecfefb3bbd6ee5e4847
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-30 00:17:42 +0530

    Bank account

--- src/Bank111/BankAccount.kt
+++ src/Bank111/BankAccount.kt
@@ -1,20 +1,41 @@
 package Bank111
 
-class BankAccount(var accountHolder : String , var balance: Double) {
+class BankAccount(
+    var accountHolder : String ,
+    var balance: Double) {
 
     private val  transactionHistory =  mutableListOf<String>()
 
 
     fun deposit(amount : Double){
-
+        balance +=  amount
+        //balance = balance +amount
+        transactionHistory.add("$accountHolder deposited $$amount")
     }
 
     fun withdraw(amount: Double){
-
+        if (amount <= balance) {
+            // we can withdraw
+            balance -=  amount
+            // balance = balance -amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        }else{
+            //  we cannot withdraw money
+            println("You  don't have the funds to withdraw $$amount ")
+        }
     }
 
     fun displayTransactionHistory(){
+println("Transaction history for $accountHolder")
+     for (transaction in transactionHistory){
+         println(transaction)
+     }
 
     }
+
+    fun acctBalance (): Double{
+        return balance
+    }
+
 
 }
