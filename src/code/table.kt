2af4668eee96129abb0d77d331299720e9fe0504
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-01 00:11:34 +0530

    table

---
+++ src/code/table.kt
@@ -0,0 +1,12 @@
+package code
+
+fun main (){
+
+    print("Enter a Number : ")
+    val  number =  readln().toInt()
+
+    for (i  in 1..10){
+        println("$number x $i = ${number*i}")
+    }
+
+}
