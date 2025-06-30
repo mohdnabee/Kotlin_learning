6d33418ad19065b97b6721ef9db715ef51d0a78b
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-01 00:10:12 +0530

    Factorial

---
+++ src/code/Factorila of a Number.kt
@@ -0,0 +1,11 @@
+package code
+
+fun  main (){
+    val  number =  5
+    var  result =  1
+
+    for (i  in 1..number){
+        result *= i
+    }
+    println("Factorail  of $number is $result")
+}
