0a834fc4e3d149932d8cbe9699ebe53bc81db6c1
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-02 19:01:06 +0530

    swap  numbers

--- src/code/swap  two   numbers.kt
+++ src/code/swap  two   numbers.kt
@@ -1,13 +1,14 @@
 package code
 
 fun  main  (){
-
-
+    print("Enter The Value Of a: ")
     var a = readln().toInt()
+    print("Enter The Value Of b: ")
     var b = readln().toInt()
 
     a =  a+b
     b =  a-b
     a =  a-b
-println("after swap: a =  $a , b =  $b")
+
+    println("after swap: a =  $a , b =  $b")
 }
