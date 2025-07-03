0e5dbe8f45d4a184014780cc5fc30cbbd374c6bf
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-03 17:52:57 +0530

    copy keyword

---
+++ src/code/basic2.kt
@@ -0,0 +1,17 @@
+package code
+
+import java.awt.Color
+
+fun  main  (){
+
+
+val  blueRoseVase =  Vase("Blue","Rose")
+    val  redRoseVase =  blueRoseVase.copy(color = "Red")
+    println(blueRoseVase )
+    println(redRoseVase)
+}
+
+
+data class Vase (val  color: String ,  val  design: String){
+
+}
