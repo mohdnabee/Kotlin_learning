2c2761ada97cb7b7e77b72f9fa2c9d64b605e964
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-05 00:15:34 +0530

    call  class

--- src/code/Inhetance/Start.kt
+++ src/code/Inhetance/Start.kt
@@ -3,10 +3,21 @@ package code.Inhetance
 fun  main(){
 
 
-    val  obj1= BaseClass()
-    obj1.coreValues()
+//    val  obj1= BaseClass()
+//    obj1.coreValues()
 
-    val  obj2 = Secondary()
-    obj2.coreValues()
+//    val  obj2 = Secondary()
+//    obj2.role()
+//
+//
+//    val  obj3 = Tertiary()
+//    obj3.role()
 
+
+    var  frank  = Offspring()
+    frank.archery()
+    frank.role()
+    frank.coreValues()
+    frank.sing()
 }
+
