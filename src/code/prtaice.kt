3cef42c72285d8e14d678b781fba60702e087483
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-04 18:42:05 +0530

    pratice q

---
+++ src/code/prtaice.kt
@@ -0,0 +1,13 @@
+package code
+
+fun  main(){
+
+    print("Enter the Integer 1 : ")
+    val  num1 =  readln().toInt()
+    print("Enter the Integer 2 : ")
+    val  num2 = readln().toInt()
+
+
+
+
+}
