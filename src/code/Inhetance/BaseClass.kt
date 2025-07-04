055d116a7cec4aa103208060204bc61416a115c2
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-05 00:15:20 +0530

    parent class

--- src/code/Inhetance/BaseClass.kt
+++ src/code/Inhetance/BaseClass.kt
@@ -2,6 +2,12 @@ package code.Inhetance
 
  open class BaseClass {//  parent class
 
+
+   open  fun  role(){
+         println("Member of the House of BaseClass ")
+     }
+
+
  fun  coreValues() {
      println("Core values of house Baseclass")
  }
