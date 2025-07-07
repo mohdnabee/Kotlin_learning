1045b63a177056b0f8b7211bc3d999cbd3035b1f
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-07 18:46:04 +0530

    using when

--- src/Conditional/WhenC/opertaions.kt
+++ src/Conditional/WhenC/opertaions.kt
@@ -2,8 +2,12 @@ package Conditional.WhenC
 
 fun  main(){
 
+    println("enter Number One")
     val  num1 =  readln().toInt()
+    println("enter Number two ")
     val  num2 =  readln().toInt()
+
+
 
 
 }
