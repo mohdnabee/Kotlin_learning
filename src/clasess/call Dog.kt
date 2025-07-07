087d10f580b01b2d5ff10a273159ca1c06cc4928
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-07 18:42:46 +0530

    call  dog calls

--- src/clasess/call Dog.kt
+++ src/clasess/call Dog.kt
@@ -5,7 +5,9 @@ fun main (){
     //  creating an object/instance of the class dog
     var myDog = Dog("Rock" ,"Husky",4)
     println("${myDog.name } is a ${myDog.breed} add is ${myDog.age} year is old")
+
     println()
+
     println("A year has passed!")
     myDog.name =  "Brock"
     myDog.age=  6
