0aa8eef6912fec021005db38546840017f01972b
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-07 18:47:17 +0530

    using when

--- src/Conditional/WhenC/opertaions.kt
+++ src/Conditional/WhenC/opertaions.kt
@@ -1,11 +1,16 @@
 package Conditional.WhenC
 
 fun  main(){
+    println("enter The Integer Value Only ")
 
     println("enter Number One")
     val  num1 =  readln().toInt()
     println("enter Number two ")
     val  num2 =  readln().toInt()
+
+    println("Enter The Operators: / , + , -, *")
+
+
 
 
 
