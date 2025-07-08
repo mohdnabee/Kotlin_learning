d8afa2bead62bb67b230452ae1432dd9a55bff48
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-08 12:35:12 +0530

    using while

--- src/Range/WhlieCalculator.kt
+++ src/Range/WhlieCalculator.kt
@@ -1,3 +1,12 @@
 package Range
 
-fun main(){}
+fun main(){
+
+    println("Enter The Number 1 :  ")
+    val  num1 =  readln().toInt()
+    println("Enter The Number 2 : ")
+    val  num2 = readln().toInt()
+
+
+
+}
