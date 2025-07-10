7c49792e8d65d3910ff8e593fa9baba3c9bf587c
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-10 10:08:12 +0530

    Currency  Converter

---
+++ src/Currency Converter.kt
@@ -0,0 +1,23 @@
+fun  main (){
+
+    println("Welcome to currency  Converter (INR)")
+
+    print("Enter The Amount in INR: ₹")
+    val  inrAmount =  readln().toDoubleOrNull()
+
+    if (inrAmount != null && inrAmount >0){
+        val usd = inrAmount * 0.012
+        val eur = inrAmount * 0.011
+        val gbp = inrAmount * 0.0096
+        val jpy = inrAmount * 1.73
+
+        println("\n=== Convert Amounts ===")
+        println("USD: \$$usd")
+        println("EUR: €$eur")
+        println("GBP: £$gbp")
+        println("JPY: ¥$jpy")
+    }else{
+        println("Invalid input. Please enter a positive number.")
+    }
+
+}
