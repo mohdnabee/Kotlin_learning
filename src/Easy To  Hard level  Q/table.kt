453d487dbf59655705995477e0c412f6e6ab57a5
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-12 10:05:40 +0530

    table

---
+++ src/Easy To  Hard level  Q/table.kt
@@ -0,0 +1,11 @@
+fun  main  (){
+
+    print("enter The number to  print a table ")
+    val  number = readln().toInt()
+
+    for (i in  1..10){
+        println("$number x $i =  ${number * i}")
+    }
+
+
+}
