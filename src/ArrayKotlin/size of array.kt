fd874ec4f2e9d969a0e2cc9cf5f37c375571c8df
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-15 19:51:09 +0530

    size of array

---
+++ src/ArrayKotlin/size of array.kt
@@ -0,0 +1,15 @@
+package ArrayKotlin
+
+fun  main  (){
+
+    val  numbers =  arrayOf(30,40,70,80,100)
+
+    println("size of the array : ${numbers.size}")
+
+}
+
+//  ❌ .length is NOT used with Kotlin arrays
+//length is used in Java or with strings in Kotlin.
+
+//   property ||   Example
+//  .size     || arr.size
