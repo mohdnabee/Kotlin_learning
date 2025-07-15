a0fed729efc9c375ea3253e9c1fcde86f3dc48cd
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-15 19:49:59 +0530

    print all  elements

---
+++ src/ArrayKotlin/print all  elements.kt
@@ -0,0 +1,14 @@
+package ArrayKotlin
+
+fun  main (){
+
+    // Declare and initialize an array of 5 integers
+    val  numbers =  arrayOf(10,20,30,50,60)
+
+    print ("The elements of an  array  are :")
+
+    for (num  in  numbers){
+        println(num)
+    }
+
+}
