8c65bdf6c7fc574cbba2fe3a6183c241991e2750
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-19 20:28:27 +0530

    data class

---
+++ src/classData/android course.kt
@@ -0,0 +1,18 @@
+package classData
+
+data class CourseName(val part: Int, val name: String)
+
+fun main() {
+
+    val courseList = listOf(
+        CourseName(1, "Basics & UI Foundation"),
+        CourseName(2, "Intermediate Concepts"),
+        CourseName(3, "APIs & Advanced UI"),
+        CourseName(4, "Projects (Mini + Full Clones)")
+    )
+
+    // Example: Print the course list
+    for (course in courseList) {
+        println("Part ${course.part}: ${course.name}")
+    }
+}
