5249990dc4ff8b17125c7426fc7d7a4aea49807e
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-20 11:22:33 +0530

    mutableListOf

---
+++ src/classData/mutable list  add and remove.kt
@@ -0,0 +1,16 @@
+package classData
+
+fun main(){
+
+    val  number =  mutableListOf(10,20,30,40)
+
+    //  add number
+    number.add(50)
+
+    println(number)
+
+    //  remove number
+    number.remove(20)
+    println(number)
+
+}
