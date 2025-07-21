c59e4bb8c98e5651bbb437f43eee2b7b9174b901
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-21 09:28:37 +0530

    mutableListOf

---
+++ src/classData/mutale details.kt
@@ -0,0 +1,13 @@
+package classData
+
+fun  main  (){
+
+   val  pc =   mutableListOf("Ram", "SSD", "Graphic card", "MotherBoard", "Headphone","Keyboard","Mouse")
+
+    println("---------------Buy a Gaming PC----------------------")
+
+    for (list  in  pc){
+        println(list[0])
+    }
+
+}
